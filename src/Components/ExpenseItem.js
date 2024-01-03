9d359593418b87dd8b60fa6d1d49636ec5c72329
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-01-04 01:25:34 +0530

    commit one

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,18 @@
+import React, {useState} from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 function ExpenseItem(props){
+    const [title, setTitle] = useState(props.title);
+    let del = () =>{
+        setTitle("update title!");
+    }
     return(
         <Card className="SmallBox">
             <ExpenseDate date = {props.date}/>
-            <p className="title">{props.title}</p>
+            <p className="title">{title}</p>
             <Card className="amount">/-{props.amount}</Card>
+            <button onClick = {del}>Delete</button>
         </Card>
     );
 }

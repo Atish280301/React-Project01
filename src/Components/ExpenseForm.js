90cefa2ec92cd78d63b3eb65a77e7b581e5fb0b5
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-01-04 18:36:21 +0530

    commit five

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,29 +1,53 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 function ExpenseForm(){
-    const [etitle, SetTitle] = useState("");
-    const [eamount, SetAmount] = useState("");
-    const [edate, SetDate] = useState("");
-    const func = (e) => {
-        e.preventDefault();
-        console.log("Title : ",etitle);
-        console.log("Amount : ",eamount);
-        console.log("Date : ",edate);
+    //Traditional Method Use for each input at a single point of time
+    // const [enteredTitle, SetEnteredTitle] = useState('');
+    // const [enteredAmount, SetEnteredAmount] = useState('');
+    // const [enteredDate, SetEnteredDate] = useState('');
 
-        SetTitle("");   SetAmount("");  SetDate("");
+    //Alternative method use object and spread operator
+    const [userInput, SetUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
+    const titleChangeHandler = (event) => {
+        //SetEnteredTitle(event.target.value);
+        SetUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        })
+    };
+
+    const amountChangeHandler = (event) => {
+        // SetEnteredAmount(event.target.value);
+        SetUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        })
+    }
+
+    const dateChangeHandler = (event) =>{
+        // SetEnteredDate(event.target.value);
+        SetUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        })
     }
     return (
-        <form onSubmit = {func}>
+        <form>
             <div>
                 <label>Title:</label>
-                <input type="text" id="et" value = {etitle} onChange={(e) => SetTitle(e.target.value)}/>
+                <input type="text" onChange = {titleChangeHandler} />
             </div>
             <div>
                 <label>Amount:</label>
-                <input type="number" id= "ea" value = {eamount} onChange={(e) => SetAmount(e.target.value)}/>
+                <input type="number" id= "ea" onChange = {amountChangeHandler} />
             </div>
             <div>
                 <label>Date:</label>
-                <input type="date" min="2023-01-01" max="2024-12-31" id="ed" value={edate} onChange = {(e) => SetDate(e.target.value)}/>
+                <input type="date" min="2023-01-01" max="2024-12-31" id="ed" onChnage = {dateChangeHandler} />
             </div>
             <div>
                 <button type="submit">Submit Expense!</button>

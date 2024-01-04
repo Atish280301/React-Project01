5bb323d43fb3661407a3d9923ab0003d3c61d0df
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-01-04 23:00:28 +0530

    Commit nine

--- src/Components/Expense.js
+++ src/Components/Expense.js
@@ -1,9 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import "./Expense.css";
 import Card from "./Card";
+import { useState } from "react";
 function Expense(props){
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChnageHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
     return (
         <Card className="Big-Box">
+            <ExpenseFilter selected = {filteredYear} onChangeFilter={filterChnageHandler} />
             <ExpenseItem title = {props.expense[0].title} amount = {props.expense[0].amount} date = {props.expense[0].date}></ExpenseItem>
             <ExpenseItem title = {props.expense[1].title} amount = {props.expense[1].amount} date = {props.expense[1].date}></ExpenseItem>
             <ExpenseItem title = {props.expense[2].title} amount = {props.expense[2].amount} date = {props.expense[1].date}></ExpenseItem>

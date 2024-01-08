4d5060de053486403480a024527639da6813167d
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-01-08 15:04:32 +0530

    TO DO - 02
    Add condition if in input field there is no data then the color will change into red and you are not allowed to add data.

--- ToDo Project/TO DO React/src/components/CourseGoals/CourseInput/CourseInput.js
+++ ToDo Project/TO DO React/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
-
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
-
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [IsValid, SetIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      SetIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      SetIsValid(false);
+      return; 
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style = {{color: !IsValid ? 'red' : 'black'}}>Course Goal</label>
+        <input 
+          style = {
+            {border: !IsValid ? '2px solid red' : '2px solid black'}
+          } 
+          type="text" onChange={goalInputChangeHandler} 
+          />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
